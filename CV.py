49cdd8d101cfef8b5246df370f83f2325dc7cb44
Alexey Tretyakov <48726736+ATreTCM@users.noreply.github.com>
2021-05-14 16:51:02 +0300

    Add files via upload

---
+++ CV.py
@@ -0,0 +1,21 @@
+class Candidate:
+
+    def __init__(self):
+        self.firstName = 'Tretyakov'
+        self.name = 'Alexey'
+        self.age = '33'
+        self.contact = {
+            'phone': '0965380490',
+            'email': 'atret1988@gmail.com',
+            'github': 'https://github.com/ATreTCM'
+        }
+        self.hardSkills = [
+            'Python 3', 'Django', 'Django REST framework',
+            'MySQL', 'PostgreSQL', 'BeautifulSoup',
+            'Requests', 'Celery/Redis', 'HTML/CSS'
+        ]
+        self.softSkills = [
+            'communicable', 'stress-resistant', 'self-organized',
+            'punctuality', 'purposefulness', 'easily motivated'
+        ]
+        self.english = 'pre-intermediate'
